010e75d0fc0f743a59a457864c83a7cc1dd0484c
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2024-10-16 15:02:02 +0700

    add verify account

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,36 +1,34 @@
 import { createContext, useContext, useState, useEffect } from "react";
-
-import { useDispatch, useSelector } from 'react-redux'
-
+import { useDispatch, useSelector } from 'react-redux';
 
 const AuthContext = createContext();
-
 
 export function useAuth() {
   return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }) {
-  const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
   const [currentUser, setCurrentUser] = useState(userInfo);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
-
-  useEffect( () => {
-      setCurrentUser(userInfo);  
-  }, []);
+  useEffect(() => {
+    setCurrentUser(userInfo);  
+    setLoading(false);
+  }, [userInfo]);
 
   const value = {
     currentUser,
     error,
     setError,
+    loading, 
   };
 
   return (
     <AuthContext.Provider value={value}>
-      {children}
+      {!loading && children}
     </AuthContext.Provider>
   );
 }

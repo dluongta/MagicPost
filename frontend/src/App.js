d5336437f2d487e17f84963e8a85fba765ac92ac
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2024-10-16 15:08:10 +0700

    add verify account

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
-  Routes
+  Routes,
+  Route
 } from "react-router-dom";
-import Header from './component/Header'
+import Header from './component/Header';
 import Homepage from './Homepage';
 import Login from './Login';
 import Search from './Search';
 import Register from './Register';
 import LatestPosts from './component/LatestPost';
 import Post from './component/Post';
-import SearchBar from './component/SearchBar';
 import PostManage from './PostManage';
 import PostEdit from './PostEdit';
 import { AuthProvider } from './contexts/AuthContext';
-import ChatLayout from './component/ChatLayout'
+import ChatLayout from './component/ChatLayout';
 import WithPrivateRoute from './utils/WithPrivateRoute';
 import ProfileScreen from './ProfileScreen';
 import UserEditScreen from './UserEditScreen';
@@ -30,42 +28,38 @@ import VerifyPage from './VerifyPage';
 import AccountVerified from './AccountVerified';
 
 function App() {
-  return (  
-    <>
+  return (
     <AuthProvider>
-    <Router>
-    <Routes>
-    <Route path="/" element={<Homepage />} />
-    <Route path='/login' element={<Login/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/posts/:id'element={<Post/>} />
-      <Route path='/search/:keyword' element={<LatestPosts/>}  />
-      <Route path='/search' element={<Search/>} exact />
-      <Route path='/admin/postlist' element={<PostManage/>} exact />
-      <Route path='/admin/post/:id/edit' element={<PostEdit/>} />
-      <Route path='/profile' element={<ProfileScreen/>} />
-      <Route path='/admin/userlist' element={<UserListScreen/>} />
-      <Route path='/admin/user/:id/edit' element={<UserEditScreen/>} />
-      <Route path='/receipt' element={<ReceiptPrint/>} />
-      <Route path='/forgot-password' element={<ForgotPassword/>} />
-      <Route path='/reset-password/:id/:token' element={<ResetPassword/>} />
-      <Route path="/account-verified" element={<AccountVerified/>} />
-      <Route path="/verify-page" element={<VerifyPage/>} />
-      <Route
-            exact
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/posts/:id' element={<Post />} />
+          <Route path='/search/:keyword' element={<LatestPosts />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/admin/postlist' element={<PostManage />} />
+          <Route path='/admin/post/:id/edit' element={<PostEdit />} />
+          <Route path='/profile' element={<ProfileScreen />} />
+          <Route path='/admin/userlist' element={<UserListScreen />} />
+          <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
+          <Route path='/receipt' element={<ReceiptPrint />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password/:id/:token' element={<ResetPassword />} />
+          <Route path="/account-verified" element={<AccountVerified />} />
+          <Route path="/verify-page" element={<VerifyPage />} />
+          <Route
             path="/chat"
             element={
               <WithPrivateRoute>
-              <ChatLayout />
+                <ChatLayout />
               </WithPrivateRoute>
             }
-        />
-        <Route path='/print/:id' element={<InvoicePrint/>}/>
-
-    </Routes>
-    </Router>
+          />
+          <Route path='/print/:id' element={<InvoicePrint />} />
+        </Routes>
+      </Router>
     </AuthProvider>
-    </>
   );
 }
 
